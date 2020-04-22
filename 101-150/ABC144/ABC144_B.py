18b0223da5b3720419c9ae142d8428018079a7b4
Karuka3 <karuka.piplup.0327@gmail.com>
2020-04-22 14:38:36 +0900

    Fix ABC 144 B

--- 101-150/ABC144/ABC144_B.py
+++ 101-150/ABC144/ABC144_B.py
@@ -1,10 +1,10 @@
 N = int(input())
 k = False
 for a in range(1, 10):
-    for b in range(1, 10):
-        if a * b == N:
+    if N % a == 0:
+        b = int(N / a)
+        if b > 0 and b < 10:
             k = True
-            break
 if k:
     print("Yes")
 else:
